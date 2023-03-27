fe78946704decdfed66c6b206e23a3796ed1fd17
Lewis Muthaura <lewislemmu10@gmail.com>
2023-03-27 23:59:49 +0300

    section

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,11 +2,17 @@ import React from 'react'
 import "./Home.style.css";
 const Home = () => {
   return (
+    <>
     <article className='article-hearder'>
       <header>
         <h1>React: Simple CRUD Application</h1>
       </header>
     </article>
+
+    <section>
+      <div>section</div>
+    </section>
+    </>
   )
 }
 
