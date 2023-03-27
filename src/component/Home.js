2ce5d774028f7cf7d03dd9d426c62ff2147870dc
Lewis Muthaura <lewislemmu10@gmail.com>
2023-03-27 23:44:17 +0300

    navigation file

---
+++ src/component/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <article>
+      <header>
+        <h1>React: Simple CRUD Application</h1>
+      </header>
+    </article>
+  )
+}
+
+export default Home
